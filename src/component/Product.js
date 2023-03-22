5d37122c58788778672868544ca1980c60c439bc
Rojarajangit <rojrajan27@gmail.com>
2023-03-22 20:34:49 +0530

    id

--- src/component/Product.js
+++ src/component/Product.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 export default function Product() {
-  return (
+   const { id } = useParams()
+
+
+   return (
     <div>
+        <h1>{id}</h1>
       <h1>Product</h1>
       
     </div>
