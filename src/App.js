41534cb63be9516bc645f546e30dde9fb668df6f
Rojarajangit <rojrajan27@gmail.com>
2023-03-22 20:34:20 +0530

    id

--- src/App.js
+++ src/App.js
@@ -8,16 +8,9 @@ import Product from './component/Product'
 export default function App() {
   return (
     <div>
-    <Link to="/">Home</Link><br/>
-    <Link to="/cart">Cart</Link><br/>
-    <Link to="/product">Product</Link><br/>
-    <Link to="/product/TV">TV</Link>
-     <Link to="/product/SP">SP</Link>
-
     <Header/>
       <Routes>
-        
-        <Route 
+         <Route 
           path='/'
            element={<Home/>}
            />
@@ -25,24 +18,8 @@ export default function App() {
          path='/cart' 
          element={<Cart/>}
          />
-         //nested route
-      <Route path='/product'>
-      
-        <Route index element={<product/>}/>
-
-        <Route path='TV' >
-              <Route index element={<h1>TV</h1>}/>
-
-               <Route path='flat' element={<h1>flat TV</h1>}/>
-               
-               
-
-        </Route>
-        <Route path='SP' element={<h1>SP</h1>}/>
-
-      </Route>
-      <Route path='/*' element={<h1>404</h1>}/>
-
+       <Route path='/:id' element={<Product />}/>
+       
       </Routes>
       
     </div>
